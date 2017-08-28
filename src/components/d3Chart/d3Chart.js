dbf9ad19010a5defc1f5c47e77e04ed926bb2060
Odin Thorsen <odin.thorsen@genus.no>
2017-08-28 13:23:21 +0200

    4 MODULES COMPLETE

---
+++ src/components/d3Chart/d3Chart.js
@@ -0,0 +1,15 @@
+import React, {PropTypes} from 'react';
+
+const d3Chart = (props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+d3Chart.propTypes = {
+    // myProp: PropTypes.string.isRequired
+};
+
+export default d3Chart;
---
+++ src/docs/examples/HelloWorld/HelloWorld.js
@@ -0,0 +1,6 @@
+import HelloWorld from '../../../components/HelloWorld/HelloWorld';
+import * as React from "react";
+
+export default function ExampleHelloWorld() {
+    return <HelloWorld message="Wubbalubbadubdub!" />
+}
